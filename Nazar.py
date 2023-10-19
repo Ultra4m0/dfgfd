a1979fb9c23f28daad14eb545da6b25e35261a2f
Ultra4m0 <148421445+Ultra4m0@users.noreply.github.com>
2023-10-19 11:07:10 +0600

    Add files via upload

---
+++ Nazar.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
